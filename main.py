fcfc56f5f55308cd5be9a4c9932d1fa94366574c
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-09-16 19:33:17 +0200

    can create a window

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import moderngl_window as mglw
+
+
+class MainWindow(mglw.WindowConfig):
+    gl_version = (3, 3)
+    title = "CPRenderer"
+    window_size = (1280, 720)
+    aspect_ratio = None
+    resizable = False
+    vsync = True
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def on_render(self, time: float, frame_time: float):
+        self.ctx.clear(1.0, 0.0, 0.0, 0.0)
+
+
 def main():
-    print("Hello from cprenderer!")
+    # stage = Usd.Stage.Open("assets/scifi_room/scene.usdc")
+    # meshes = []
+    # for prim in stage.Traverse():
+    #     if prim.IsA(UsdGeom.Mesh):
+    #         meshes.append(prim)
+
+    # print(meshes)
+
+    MainWindow.run()
 
 
 if __name__ == "__main__":
